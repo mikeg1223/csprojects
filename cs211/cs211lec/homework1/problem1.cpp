5710389c176b6d862f07d634a5138f980f5f096f
Mike <michael.grossman066@qmail.cuny.edu>
2020-01-28 21:59:31 -0500

    to add hw

---
+++ cs211/cs211lec/homework1/problem1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+bool isPrime(int m);
+void printPrimes();
+
+int main()
+{
+	
+}
+
+bool isPrime(int m)
+{
+	int x = sqrt(m)
+	for(int i = 2; i <= x; ++i)
+	{
+		if(!(m%x))
+		{
+			return false;
+		}
+	}
+	return true;
+}
+void printPrimes()
+{
+	int n = 0;
+	while(n < 1)
+	{
+	std::cout << "Please enter a number to print until: ";
+	std::cin >> n;
+	}
+	for(int i = 2; i <= 
+}
